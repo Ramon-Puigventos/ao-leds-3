d65963a05162ac1caf1699ad8cbe3f0455402b9e
Ramon-Puigventos <146018688+Ramon-Puigventos@users.noreply.github.com>
2024-11-17 18:32:30 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+        n += 1
+        basic.showNumber(n)
+    }
+})
+let n = 0
+n = 0
+basic.forever(function () {
+	
+})
